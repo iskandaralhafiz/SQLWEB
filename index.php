a9863f7ad8f63734b5070b790c2c87ec0f554690
iskandaralhafiz <iskhan_alhafiz@yahoo.com>
2016-06-22 13:49:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -38,19 +38,6 @@ die(print_r($e));
 
 echo "koneksi sukses"
 
-
-try {
-$Kode_Pengguna = $_POST['Kode_Pengguna'];
-$Nama_Pengguna = $_POST['Nama_Pengguna'];
-
-// Insert data
-$sql_insert = "INSERT INTO dbo.TblPengguna (Kode_Pengguna, Nama_Pengguna) VALUES ('$Kode_Pengguna', '$Nama_Pengguna')";
-$stmn = sqlsrv_query($conn, $sql_insert);  
-
- sqlsrv_free_stmt($stmn);  
- sqlsrv_close($conn);           
-
-}
 catch ( PDOException $e ){
   print( "Error connecting to SQL Server" );
 die(print_r($e));
