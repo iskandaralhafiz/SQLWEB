aae835bd05131e53d485cb8fe915c92991572e8c
iskandaralhafiz <iskhan_alhafiz@yahoo.com>
2016-06-22 10:54:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -44,12 +44,11 @@ $Kode_Pengguna = $_POST['Kode_Pengguna'];
 $Nama_Pengguna = $_POST['Nama_Pengguna'];
 
 // Insert data
-$sql_insert = "INSERT INTO dbo.TblPengguna (Kode_Pengguna, Nama_Pengguna) 
-VALUES ($Kode_Pengguna, $Nama_Pengguna)";
+$sql_insert = "INSERT INTO dbo.TblPengguna (Kode_Pengguna, Nama_Pengguna) VALUES ($Kode_Pengguna, $Nama_Pengguna)";
 $stmn = sqlsrv_query($conn, $sql_insert);  
 
  sqlsrv_execute($stmn);  
-            sqlsrv_close($conn); 
+           
 
 }
 catch ( PDOException $e ){
