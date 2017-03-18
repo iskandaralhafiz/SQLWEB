ea8983747a369b704e594e029a339efb0aca6ad7
iskandaralhafiz <iskhan_alhafiz@yahoo.com>
2017-03-19 06:35:36 +0700

    Update login.php

--- login.php
+++ login.php
@@ -12,15 +12,14 @@ $username=$_POST['username'];
 $password=$_POST['password'];
 // Membangun koneksi ke database
 //$connection = mysql_connect("localhost", "root", "");
-  Try 
-{
-$conn = new PDO ( "sqlsrv:server = tcp:quantumcom.database.windows.net,1433; Database = SIMTIFDB", "qdadmin", "Kafalahajai5654@");
-$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch ( PDOException $e ){
-  print( "Error connecting to SQL Server" );
-die(print_r($e));
-}
+  
+ $serverName = "quantumcom.database.windows.net"; 
+ $connectionInfo = array( "Database"=>"SIMTIFDB", "UID"=>"qdadmin", "PWD"=>"Kafalahajai5654@"); 
+ $conn = sqlsrv_connect( $serverName, $connectionInfo); 
+ if( $conn === false ) { 
+      die( print_r( sqlsrv_errors(), true)); 
+ } 
+
   
 // Mencegah MySQL injection 
 $username = stripslashes($username);
@@ -30,15 +29,24 @@ $password = mssql_real_escape_string($password);
 // Seleksi Database
 //$db = mssql_select_db("tes_db", $connection);
 // SQL query untuk memeriksa apakah karyawan terdapat di database?
-$query = mssql_query("select * from TblPengguna1 where password='$password' AND username='$username'", $conn);
-$rows = mssql_num_rows($query);
-if ($rows == 1) {
+//$query = mssql_query("select * from TblPengguna1 where password='$password' AND username='$username'", $conn);
+//$rows = mssql_num_rows($query);
+  
+  $sql = "SELECT * FROM TblPengguna1 WHERE password='$password' AND username='$username'"; 
+          
+ $stmt = sqlsrv_query( $conn, $sql); 
+ if( $stmt === false ) { 
+      die( print_r( sqlsrv_errors(), true)); 
+ } 
+
+  
+if ($stmt == 1) {
 $_SESSION['login_user']=$username; // Membuat Sesi/session
 header("location: profile.php"); // Mengarahkan ke halaman profil
 } else {
 $error = "Username atau Password belum terdaftar";
 }
-mssql_close($conn); // Menutup koneksi
+sqlsrv_close( $conn ); // Menutup koneksi
 }
 }
 ?>
