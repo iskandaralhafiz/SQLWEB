2001c7229f0e67c5bd2214fd3cd89f65af59283c
iskandaralhafiz <iskhan_alhafiz@yahoo.com>
2017-03-19 17:41:05 +0700

    Update form_login.php

--- form_login.php
+++ form_login.php
@@ -3,6 +3,25 @@
   <head>
     <title>3UP TEAM Form Login</title>
 	
+<meta content="text/html; charset=ISO-8859-1"
+http-equiv="content-type">
+<title>coba design1</title>
+</head>
+<body>
+<img style="width: 268px; height: 118px;" alt="" src="LOGO3.png"
+font-size:="" font-family:="" color:=""><br>
+<span
+style="font-size: 28pt; font-family: &quot;Arial Black&quot;; color: rgb(0, 176, 80);">B41
+| BENGKALIS</span><span
+style="font-size: 28pt; font-family: &quot;Arial Black&quot;; color: rgb(146, 208, 80);">
+</span><span
+style="font-size: 28pt; font-family: &quot;Arial Black&quot;; color: rgb(89, 89, 89);">KONSOLIDASI</span><span
+style="font-size: 28pt; font-family: &quot;Arial Black&quot;; color: rgb(89, 89, 89);"></span><br>
+<big style="font-family: Calibri Light; color: rgb(102, 102, 102);"><font
+size="-1"><big>www.3upteam.com|e-mail:iskhan_alhafiz@yahoo.com|WA:
++6281363120816|Penakluk|Petarung| Pemeang</big></font></big><br>
+<br>	    
+	  
 	<!-- Skrip CSS -->
    <link rel="stylesheet" href="style.css"/>
   
