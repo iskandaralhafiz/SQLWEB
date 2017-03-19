5258edb1bea4e799af40e115661607ac5b0c07a7
iskandaralhafiz <iskhan_alhafiz@yahoo.com>
2017-03-19 14:37:20 +0700

    Update form_login.php

--- form_login.php
+++ form_login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Nyekrip Form Login</title>
+    <title>3UP TEAM Form Login</title>
 	
 	<!-- Skrip CSS -->
    <link rel="stylesheet" href="style.css"/>
@@ -11,7 +11,7 @@
 	<div class="container">
 		<div class="main">
 	      <form action="proses_login.php" method="post">
-			<h2>NYEKRIP.COM FORM LOGIN DENGAN PHP</h2><hr/>		
+			<h2>LOGIN 3UP TEAM</h2><hr/>		
 			
 			<label>Username :</label>
 			<input id="name" name="username" placeholder="username" type="text">
