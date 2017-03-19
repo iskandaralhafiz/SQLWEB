e22603c2e6a75ad3c20bc34dabeb87d7ba5a149e
iskandaralhafiz <iskhan_alhafiz@yahoo.com>
2017-03-19 18:03:26 +0700

    Update form_login.php

--- form_login.php
+++ form_login.php
@@ -8,16 +8,15 @@ http-equiv="content-type">
 <title>coba design1</title>
 </head>
 <body>
-<img style="width: 268px; height: 118px;" alt="" src="LOGO3.png"
+<img style="width: 862px; height: 192px;" alt="" src="logofinal.PNG"
 font-size:="" font-family:="" color:=""><span
-
 style="font-size: 28pt; font-family: &quot;Arial Black&quot;; color: rgb(0, 176, 80);"></span><span
-style="font-size: 28pt; font-family: &quot;Arial Black&quot;; color: rgb(89, 89, 89);">BENGKALIS
-FOR INDONESIA </span><br>
-<big style="font-family: Calibri Light; color: rgb(102, 102, 102);"><font
-size="-1"><big>www.3upteam.com|e-mail:iskhan_alhafiz@yahoo.com|WA:
-+6281363120816|Penakluk|Petarung| Pemeang</big></font></big><br>
-<br>	    
+style="font-size: 28pt; font-family: &quot;Arial Black&quot;; color: rgb(89, 89, 89);"></span><span
+style="font-size: 28pt; font-family: &quot;Arial Black&quot;; color: rgb(89, 89, 89);"></span><br>
+<span
+style="font-size: 28pt; font-family: &quot;Arial Black&quot;; color: rgb(0, 176, 80);"></span><span
+style="font-size: 28pt; font-family: &quot;Arial Black&quot;; color: rgb(89, 89, 89);"></span><br>
+<br>
 	  
 	<!-- Skrip CSS -->
    <link rel="stylesheet" href="style.css"/>
