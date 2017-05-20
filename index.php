84bda103b20fea5b051e2c8df37bdc9eaadf4080
iskandaralhafiz <iskhan_alhafiz@yahoo.com>
2017-05-20 19:49:52 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>QLS INDONESIA Login</title>
+	
+<meta content="text/html; charset=ISO-8859-1"
+http-equiv="content-type">
+<title>Form Login</title>
+</head>
+<body>
+<img style="width: 862px; height: 192px;" alt="" src="logofinal1.PNG"
+font-size:="" font-family:="" color:=""><span
+style="font-size: 28pt; font-family: &quot;Arial Black&quot;; color: rgb(0, 176, 80);"></span><span
+style="font-size: 28pt; font-family: &quot;Arial Black&quot;; color: rgb(89, 89, 89);"></span><span
+style="font-size: 28pt; font-family: &quot;Arial Black&quot;; color: rgb(89, 89, 89);"></span><br>
+<span
+style="font-size: 28pt; font-family: &quot;Arial Black&quot;; color: rgb(0, 176, 80);"></span><span
+style="font-size: 28pt; font-family: &quot;Arial Black&quot;; color: rgb(89, 89, 89);"></span>
+	<!-- Skrip CSS -->
+   <link rel="stylesheet" href="style.css"/>
+  
+	</head>	
+  <body>
+	<div class="container">
+		<div class="main">
+	      <form action="proses_login.php" method="post">
+			<h2>LOGIN QLS | Dauroh Keluarga</h2><hr/>		
+			
+			<label>Username :</label>
+			<input id="name" name="username" placeholder="username" type="text">
+			
+			<label>Password :</label>
+			<input id="password" name="password" placeholder="**********" type="password">
+			
+			<input type="submit" name="submit" id="submit" value="Login">
+		  </form>
+		</div>
+   </div>
+
+  </body>
+</html>
