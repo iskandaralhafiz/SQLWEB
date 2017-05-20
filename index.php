8162765b8976c247ecab8f9b73525d4b8a4418cb
iskandaralhafiz <iskhan_alhafiz@yahoo.com>
2017-05-20 20:49:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+!DOCTYPE html>
 <html>
   <head>
     <title>QLS INDONESIA Login</title>
@@ -8,7 +8,7 @@ http-equiv="content-type">
 <title>Form Login</title>
 </head>
 <body>
-<img style="width: 300px; height: 70px;" alt="" src="logofinal1.PNG"
+<img style="width: 350px; height: 80px;" alt="" src="logofinal1.PNG"
 font-size:="" font-family:="" color:=""><span
 style="font-size: 28pt; font-family: &quot;Arial Black&quot;; color: rgb(0, 176, 80);"></span><span
 style="font-size: 28pt; font-family: &quot;Arial Black&quot;; color: rgb(89, 89, 89);"></span><span
