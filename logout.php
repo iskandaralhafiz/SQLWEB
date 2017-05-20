d35824b817c0dc6eb502a2d3ad0a33d51f328557
iskandaralhafiz <iskhan_alhafiz@yahoo.com>
2017-05-20 19:53:44 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
  session_start(); // memulai session
  session_destroy(); // menghapus session
- header("location:form_login.php"); // mengambalikan ke form_login.php
+ header("location:index.php"); // mengambalikan ke form_login.php
  ?>
