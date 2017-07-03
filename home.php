12f699ee0bddd3d689c4fef0e403530233023366
iskandaralhafiz <iskhan_alhafiz@yahoo.com>
2017-07-03 16:10:56 +0700

    Update home.php

--- home.php
+++ home.php
@@ -51,10 +51,11 @@ td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
             </select>
              
     <!--DESA-->
+ $KECAMATAN=$_GET['KODE_KEC'];
             <select id="DESA" name="DESA">
                 <option value="">Please Select</option>
                 <?php 
-             $perintah1="SELECT KODE_DESA, DESA FROM tbl_desa INNER JOIN tbl_kecamatan ON tbl_desa.KECAMATAN = tbl_kecamatan.KODE_KEC ORDER BY KODE_DESA";
+             $perintah1="SELECT KODE_DESA, DESA FROM tbl_desa WHERE KECAMATAN=$KECAMATAN ORDER BY KODE_DESA";
                 $query1 = sqlsrv_query($conn,$perintah1);
                 while ($row1 = sqlsrv_fetch_array($query1)) {
                 ?>
