e771ae45c55c7f02cad569acb3746c4c5849ad06
iskandaralhafiz <iskhan_alhafiz@yahoo.com>
2017-07-03 19:32:21 +0700

    Update home.php

--- home.php
+++ home.php
@@ -36,8 +36,10 @@ td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
 <script type="text/javascript" src="jquery-1.4.2.min.js"></script>
  
 <form method="post" action="home.php">
+ <pre>
+
   <!--KECAMATAN-->
-<select id="tbl_kecamatan" name="tbl_kecamatan">
+  Kecamatan : <select id="tbl_kecamatan" name="tbl_kecamatan">
                 <option value="">Please Select</option>
                 <?php              $perintah="SELECT KODE_KEC, KECAMATAN FROM tbl_kecamatan ORDER BY KODE_KEC";
                 $query = sqlsrv_query($conn,$perintah);
@@ -52,8 +54,7 @@ td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
             </select>
  
  <!--DESA-->
-
-            <select id="tbl_desa" name="tbl_desa">
+Desa       : <select id="tbl_desa" name="tbl_desa">
                 <option value="">Please Select</option>
                 <?php 
              $perintah1="SELECT * FROM tbl_desa INNER JOIN tbl_kecamatan ON tbl_desa.KECAMATAN = tbl_kecamatan.KODE_KEC ORDER BY KODE_DESA";
@@ -70,8 +71,7 @@ td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
             </select>
     
  <!--TPS-->
-
-            <select id="tbl_tps1" name="tbl_tps1">
+TPS       : <select id="tbl_tps1" name="tbl_tps1">
                 <option value="">Please Select</option>
                 <?php 
              $perintah2="SELECT * FROM tbl_tps1 INNER JOIN tbl_desa ON tbl_tps1.DESA = tbl_desa.KODE_DESA ORDER BY KODE_TPS";
