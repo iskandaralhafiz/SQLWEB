0a58c005aa31b80a790e32bf164887847c1730bd
iskandaralhafiz <iskhan_alhafiz@yahoo.com>
2017-07-03 17:25:35 +0700

    Create cari_desa.php

---
+++ cari_desa.php
@@ -0,0 +1,14 @@
+<?php 
+    include "koneksi.php";
+    
+    $sel_prov="select KODE_DESA, DESA from tbl_desa where KECAMATAN='".$_POST["prov"]."'";
+    $q=sqlsrv_query($conn,$sel_prov);
+    while($data_prov=sqlsrv_fetch_array($q)){
+    
+    ?>
+        <option value="<?php echo $data_prov["KODE_DESA"] ?>"><?php echo $data_prov["DESA"] ?></option><br>
+    
+    <?php
+    }
+    ?>
+   
