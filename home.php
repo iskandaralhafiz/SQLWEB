2ba07ba102c5c466917cf1d3eec60f5b7caf4fac
iskandaralhafiz <iskhan_alhafiz@yahoo.com>
2017-07-08 23:13:18 +0700

    Update home.php

--- home.php
+++ home.php
@@ -7,8 +7,7 @@ session_start();
  ?>
  Selamat <b><?php echo $_SESSION['username'] ?></b> Berhasil Masuk <br />
  <a href="logout.php">Klik di sini</a> untuk Keluar
-<?php } ?>
-//----------------------
+<?php } ?>//----------------------
 
 <html>
 <head>
@@ -18,8 +17,7 @@ session_start();
 body { background-color: #fff; border-top: solid 10px #000;
 color: #333; font-size: .85em; margin: 20; padding: 20;
 font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
-}h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }h1 { font-size: 2em; }
-h2 { font-size: 1.75em; }
+}h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }h1 { font-size: 2em; }h2 { font-size: 1.75em; }
 h3 { font-size: 1.2em; }
 table { margin-top: 0.75em; }
 th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
@@ -28,7 +26,6 @@ td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
  </head><body>
  
 <img style="width: 300px; height: 80px;" alt="" src="dapur2.PNG"
-
 
  
  <?php
@@ -40,8 +37,7 @@ td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
 <form method="post" action="home.php"> 
    <table style="text-align: left; width: 401px; height: 194px;"
  border="0" cellpadding="2" cellspacing="2">
-    <tbody>
-      <tr>
+    <tbody>      <tr>
         <td colspan="2" rowspan="1"
  style="width: 119px; background-color: rgb(153, 0, 0);"><span
  style="color: white;">ENTRI PENJUALAN</span></td>
@@ -82,8 +78,17 @@ td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
       </tr>
       <tr>
         <td style="width: 170px;">
-        <select name="Pelanggan">
-        </select>
+        <select id="TblBarang" name="TblBarang">
+                <option value="">Please Select</option>
+                <?php              $perintah="SELECT KODE_BARANG, Nama_Barang FROM blBarangn ORDER BY KODE_BARANG";
+                $query = sqlsrv_query($conn,$perintah);
+                while ($row = sqlsrv_fetch_array($query)) {
+                ?>
+                    <option value="<?php echo $row['KODE_BARANG']; ?>">
+                        <?php echo $row['Nama_Barang']; ?>
+                    </option>                <?php
+                }
+                ?>
         <br>
         </td>
         <td style="width: 79px;"><input size="15"
