27b49ec0c9b5bb43e30730da6467514acf2e4f51
iskandaralhafiz <iskhan_alhafiz@yahoo.com>
2017-07-09 09:43:17 +0700

    Update home.php

--- home.php
+++ home.php
@@ -165,11 +165,10 @@ td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
   // print( "Error connecting to SQL Server" );
 //die(print_r($e));
 
-if(isset($_POST['NoFaktur']))
+if(isset($_POST[NoFaktur]))
 {
- $faktur = $_POST['NoFaktur'];
+ echo $_POST[NoFaktur];
 
- echo "$faktur";
 }
 
 
