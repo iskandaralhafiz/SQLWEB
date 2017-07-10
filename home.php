7a273200b3560918b74791b87b796652abea8f7c
iskandaralhafiz <iskhan_alhafiz@yahoo.com>
2017-07-10 19:26:00 +0700

    Update home.php

--- home.php
+++ home.php
@@ -53,7 +53,7 @@ $perintah="SELECT KODE_KATEGORI, KATEGORI FROM TblKategori ORDER BY KODE_KATEGOR
                 ?>
                     <option value="<?php echo $row['KODE_KATEGORI']; ?>">
                         <?php echo $row['KATEGORI']; ?>
-                    </option>                
+                   </option>                
 					<?php
              }
                 ?>
@@ -91,7 +91,7 @@ $perintah="SELECT KODE_KATEGORI, KATEGORI FROM TblKategori ORDER BY KODE_KATEGOR
  
 $KODE = $_POST['KODE_KATEGORI'];
 		
-$perintah1="SELECT KODE_BARANG, Nama_Barang,Harga_Jual,Stock FROM TblBarang WHERE Kategori= '$KODE'";
+$perintah1="SELECT KODE_BARANG, Nama_Barang,Harga_Jual,Stock FROM TblBarang WHERE Kategori= $KODE";
 
                 $query1 = sqlsrv_query($conn,$perintah1);
                 while ($row1 = sqlsrv_fetch_array($query1)) {
