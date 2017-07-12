5c63f4806b1f61d81e3319eb215f4c43992a3bb0
iskandaralhafiz <iskhan_alhafiz@yahoo.com>
2017-07-12 23:46:43 +0700

    Update entripenjualan.php

--- entripenjualan.php
+++ entripenjualan.php
@@ -1,109 +1,32 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-    <meta content="text/html; charset=ISO-8859-1"
- http-equiv="content-type">
-  <title>penjualan.php</title>
-</head>
-<body>
-<img style="width: 340px; height: 75px;" alt=""
- src="dapur2.PNG"><br>
-<form method="post" action="masterpenjualan.php"
- name="master_penjualan">
-  <table style="text-align: left; width: 401px; height: 194px;"
- border="0" cellpadding="2" cellspacing="2">
-    <tbody>
-      <tr>
-        <td colspan="2" rowspan="1"
- style="width: 119px; background-color: rgb(153, 0, 0);"><span
- style="color: white;">ENTRI PENJUALAN</span></td>
-      </tr>
-      <tr>
-        <td style="width: 119px;">No Faktur</td>
-        <td style="width: 297px;"><input size="30"
- name="NoFaktur"></td>
-      </tr>
-      <tr>
-        <td style="width: 119px;">Tanggal</td>
-        <td style="width: 297px;"><input size="30"
- name="Tanggal"></td>
-      </tr>
-      <tr>
-        <td style="width: 119px;">Pelanggan</td>
-        <td style="width: 297px;">
-        <select name="Pelanggan">
-        </select>
-        </td>
-      </tr>
-      <tr>
-        <td colspan="2" rowspan="1"
- style="width: 119px;"><input name="submit"
- value="Reset Form Penjualan" type="reset"></td>
-      </tr>
-    </tbody>
-  </table>
-  <br>
-  <hr style="width: 100%; height: 2px;">
-  <table style="text-align: left; width: 620px; height: 60px;"
- border="0" cellpadding="2" cellspacing="2">
-    <tbody>
-      <tr>
-        <td style="width: 170px;">Nama Barang</td>
-        <td style="width: 79px;">Harga @</td>
-        <td style="width: 63px;">Qty</td>
-        <td style="width: 103px;">Jumlah</td>
-        <td style="width: 149px;"></td>
-      </tr>
-      <tr>
-        <td style="width: 170px;">
-        <select name="Pelanggan">
-        </select>
-        <br>
-        </td>
-        <td style="width: 79px;"><input size="15"
- name="Harga"></td>
-        <td style="width: 63px;"><input size="5"
- name="qty"></td>
-        <td style="width: 103px;"><input size="15"
- name="total"></td>
-        <td style="width: 149px;"><input name="submit"
- value="Tambah" type="submit"></td>
-      </tr>
-    </tbody>
-  </table>
-  <hr style="width: 100%; height: 2px;"></form>
-<table
- style="text-align: left; background-color: white; width: 715px;"
- border="1" cellpadding="2" cellspacing="2">
-  <tbody>
-    <tr>
-      <th style="background-color: rgb(153, 0, 0);"><span
- style="color: white;">No.</span></th>
-      <th style="width: 171px; background-color: rgb(153, 0, 0);"><span
- style="color: white;">Nama Barang</span></th>
-      <th style="width: 125px; background-color: rgb(153, 0, 0);"><span
- style="color: white;">Harga @</span></th>
-      <th style="width: 139px; background-color: rgb(153, 0, 0);"><span
- style="color: white;">Qty</span></th>
-      <th style="width: 206px; background-color: rgb(153, 0, 0);"><span
- style="color: white;">Jumlah</span></th>
-    </tr>
-    <tr>
-      <td></td>
-      <td style="width: 171px; background-color: white;"></td>
-      <td style="width: 125px; background-color: white;"></td>
-      <td style="width: 139px; background-color: white;"></td>
-      <td style="width: 206px; background-color: white;"></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-  </tbody>
+<html><head>
+<input>
+<meta content="text/html; charset=ISO-8859-1" http-equiv="content-type"><title>Entri Penjualan</title>
+
+</head><body><img alt="" src="file:///D:/AppPenjualan/Gambar/dapur2.PNG" style="height: 75px; width: 340px" /><br />
+&nbsp;
+<form action="masterpenjualan.php" method="post" name="master_penjualan">
+<table border="0" cellpadding="2" cellspacing="2" style="height: 194px; width: 430px; text-align: left">
+	<tbody>
+		<tr>
+			<td colspan="2" style="width: 119px; background-color: rgb(153,0,0)"><span style="font-family: verdana,geneva,sans-serif"><strong><span style="color: white">ENTRI&nbsp;PENJUALAN</span></strong></span></td>
+		</tr>
+		<tr>
+			<td style="width: 119px">No Faktur</td>
+			<td style="width: 297px"><input name="NoFaktur" size="30" type="text" /></td>
+		</tr>
+		<tr>
+			<td style="width: 119px">Tanggal</td>
+			<td style="width: 297px"><input name="Tanggal" size="30" type="text" /></td>
+		</tr>
+		<tr>
+			<td style="width: 119px">Pelanggan</td>
+			<td style="width: 297px"><select name="Pelanggan"></select></td>
+		</tr>
+		<tr>
+			<td colspan="2" style="width: 119px"><input name="submit" type="submit" value="Detil Penjualan" /></td>
+		</tr>
+	</tbody>
 </table>
-<br>
-</body>
-</html>
+
+<hr style="height: 2px; width: 100%" /></form>
