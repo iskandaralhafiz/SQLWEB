a94b695ca00199c5b8c9cbc567f1be9e692688f8
iskandaralhafiz <iskhan_alhafiz@yahoo.com>
2017-07-12 23:10:12 +0700

    Create masterpenjualan.php

---
+++ masterpenjualan.php
@@ -0,0 +1,36 @@
+<div id="wb_Form1" style="position:absolute;width:662px;height:202px;">
+<form name="masterpenjualan" method="post" action="" enctype="text/plain" target="_self" id="Form1">
+<div id="wb_Text2" style="position:absolute;left:20px;top:33px;width:57px;height:16px;z-index:43;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">Tanggal</span></div>
+<input type="text" id="Editbox2" style="position:absolute;left:96px;top:59px;width:153px;height:19px;line-height:19px;z-index:44;" name="Editbox1" value="">
+<div id="wb_Text3" style="position:absolute;left:21px;top:63px;width:74px;height:16px;z-index:45;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">No. Faktur</span></div>
+<input type="text" id="Editbox3" style="position:absolute;left:95px;top:88px;width:153px;height:19px;line-height:19px;z-index:46;" name="Editbox1" value="">
+<div id="wb_Text4" style="position:absolute;left:20px;top:92px;width:74px;height:16px;z-index:47;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">Pelanggan</span></div>
+<input type="text" id="Editbox1" style="position:absolute;left:96px;top:30px;width:155px;height:19px;line-height:19px;z-index:48;" name="Editbox1" value="">
+<div id="wb_Text1" style="position:absolute;left:0px;top:0px;width:661px;height:19px;z-index:49;text-align:left;">
+<span style="color:#FFFFFF;font-family:Arial;font-size:16px;"><strong>&nbsp;&nbsp; Master Penjualan</strong></span></div>
+<div id="wb_Form2" style="position:absolute;left:2px;top:150px;width:660px;height:52px;z-index:50;">
+<form name="Form2" method="post" action="mailto:yourname@yourdomain.com" enctype="text/plain" id="Form2">
+<input type="text" id="Editbox4" style="position:absolute;left:126px;top:21px;width:144px;height:19px;line-height:19px;z-index:32;" name="Editbox1" value="">
+<select name="Combobox1" size="1" id="Combobox1" style="position:absolute;left:16px;top:21px;width:102px;height:23px;z-index:33;">
+</select>
+<input type="text" id="Editbox5" style="position:absolute;left:276px;top:21px;width:106px;height:19px;line-height:19px;z-index:34;" name="Editbox1" value="">
+<div id="wb_Text5" style="position:absolute;left:17px;top:3px;width:103px;height:16px;z-index:35;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">Cari Barang</span></div>
+<div id="wb_Text6" style="position:absolute;left:125px;top:3px;width:103px;height:16px;z-index:36;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">Nama Barang</span></div>
+<div id="wb_Text7" style="position:absolute;left:277px;top:3px;width:103px;height:16px;z-index:37;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">Harga @</span></div>
+<input type="text" id="Editbox6" style="position:absolute;left:389px;top:21px;width:49px;height:19px;line-height:19px;z-index:38;" name="Editbox1" value="">
+<div id="wb_Text8" style="position:absolute;left:389px;top:3px;width:45px;height:16px;z-index:39;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">Qty</span></div>
+<div id="wb_Text9" style="position:absolute;left:446px;top:4px;width:103px;height:16px;z-index:40;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">Jumlah</span></div>
+<input type="text" id="Editbox7" style="position:absolute;left:443px;top:21px;width:142px;height:19px;line-height:19px;z-index:41;" name="Editbox1" value="">
+<input type="submit" id="Button1" name="" value="Tambah" style="position:absolute;left:593px;top:20px;width:61px;height:25px;z-index:42;" title="Tambahkan Barang !">
+</form>
+</div>
+</form>
+</div>
